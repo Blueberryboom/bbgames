b478f084b7f0679ada98ec0c1837759db4365d9a
Blueberryboom <152311332+Blueberryboom@users.noreply.github.com>
2026-02-06 17:53:36 +0000

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'bbgames',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 5
 });
 
