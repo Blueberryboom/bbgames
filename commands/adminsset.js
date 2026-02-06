9ee07a439f6c440ae80f5a14ca022235d1029112
Blueberryboom <152311332+Blueberryboom@users.noreply.github.com>
2026-02-06 18:34:27 +0000

    Create adminsset.js

---
+++ commands/adminsset.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const pool = require('../database');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('admins set')
+    .setDescription('Set event admin role')
+    .addRoleOption(o =>
+      o.setName('role')
+       .setDescription('Role that can manage the bot')
+       .setRequired(true)
+    ),
+
+  async execute(interaction) {
+
+    if (!interaction.member.permissions.has("Administrator"))
+      return interaction.reply({
+        content: "❌ Admin only!",
+        ephemeral: true
+      });
+
+    const role = interaction.options.getRole('role');
+
+    await pool.query(
+      "REPLACE INTO event_admin_roles VALUES (?, ?)",
+      [interaction.guild.id, role.id]
+    );
+
+    await interaction.reply(
+      `✅ ${role} can now manage giveaways!`
+    );
+  }
+};
